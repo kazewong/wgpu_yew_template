10adc48b7593e2b5fd69de6f01c1b4618d388243
Kaze Wong <kazewong.physics@gmail.com>
2024-02-02 14:13:50 -0500

    Refactor app.rs to initialize WGPUContext asynchronously

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,19 @@
 use crate::context::WGPUContext;
 
-use yew::{html, Callback, Component, Context, ContextProvider, Html};
-use yew::prelude::*;
 use winit::{
-    event::WindowEvent, event_loop, window::{Window, WindowBuilder}
+    event::WindowEvent,
+    event_loop,
+    window::{Window, WindowBuilder},
 };
-use wasm_bindgen_futures::spawn_local;
+use yew::platform::spawn_local;
+use yew::prelude::*;
+use yew::{html, AttrValue, Callback, Component, Context, ContextProvider, Html};
 
 pub enum AppMsg {
+    Initializing(WGPUContext),
+    Initialized(WGPUContext),
     Redraw,
-    Nothing
+    Nothing,
 }
 
 #[derive(PartialEq, Properties, Default)]
@@ -17,7 +21,14 @@ pub struct AppProperties {}
 
 pub struct App {
     canvas: NodeRef,
-    // context: WGPUContext,
+    context: Option<WGPUContext>,
+}
+
+fn emit_context(window: Window, context_cb: Callback<WGPUContext>) {
+    spawn_local(async move {
+        let context = WGPUContext::new(window).await;
+        context_cb.emit(context);
+    });
 }
 
 impl Component for App {
@@ -25,28 +36,36 @@ impl Component for App {
     type Properties = AppProperties;
 
     fn create(ctx: &Context<Self>) -> Self {
-        ctx.link().send_message(AppMsg:: Redraw);
+        ctx.link().send_message(AppMsg::Redraw);
         let event_loop = event_loop::EventLoop::new();
         let window = WindowBuilder::new()
             .with_inner_size(winit::dpi::PhysicalSize::new(800, 600))
             .build(&event_loop)
             .unwrap();
-        // let context = spawn_local(
-        //     async move{
-        //         WGPUContext::new(window);
-        //     }
-        // );
+        let context_cb = ctx.link().callback(AppMsg::Initializing);
+        emit_context(window, context_cb);
         App {
-          canvas : NodeRef::default(),
-        //   context
+            canvas: NodeRef::default(),
+            context: None
         }
     }
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            AppMsg::Initialized(context) => {
+                self.context = Some(context);
+            }
+            AppMsg::Initializing(_) => {
+                return false;
+            }
+            AppMsg::Redraw => {
+                
+            }
+            AppMsg::Nothing => {
+                return false;
+            }
+        }
         true
-        // match msg {
-            
-        // }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
